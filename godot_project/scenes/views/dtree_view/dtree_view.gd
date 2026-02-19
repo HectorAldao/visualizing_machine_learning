87073f18368e850c85bfa1e9448e2e7436abe9ed
HectorAldao <hectoraldao@gmail.com>
2026-02-19 11:29:02 +0100

    refactor: export to web and delete zoom project of feature

--- godot_project/scenes/views/dtree_view/dtree_view.gd
+++ godot_project/scenes/views/dtree_view/dtree_view.gd
@@ -9,12 +9,10 @@ extends Control
 @export_file("*.tscn") var controller_dtree_scene: String
 
 
-@onready var scroll_container:      ScrollContainer = $ScrollContainer
-@onready var dtree_selection_panel: CenterContainer = $DTreeSelectionPanel
-@onready var subviewport_container: SubViewportContainer = $ScrollContainer/SubViewportContainer
-@onready var subviewport:           SubViewport = $ScrollContainer/SubViewportContainer/SubViewport
-@onready var dtree:                 DTreeLogical = $ScrollContainer/SubViewportContainer/SubViewport/DTree
-@onready var dtree_menu:            Control = $DTreeMenu
+@onready var scroll_container:      ScrollContainer =      $ScrollContainer
+@onready var dtree_selection_panel: CenterContainer =      $DTreeSelectionPanel
+@onready var dtree:                 DTreeLogical =         $ScrollContainer/DTree
+@onready var dtree_menu:            Control =              $DTreeMenu
 
 var dtree_controller: Node = null
 var algorithm: AlgorithmDTree = null
@@ -42,19 +40,7 @@ func _ready():
 	scroll_container.      visible = false
 	dtree_menu.            visible = false
 	
-	# Configure SubViewport
-	subviewport.transparent_bg = true
-	subviewport.size = scroll_container.size
-	
-	# Connect resize signal to update SubViewport size
-	scroll_container.resized.connect(_on_scroll_container_resized)
 
-
-func _on_scroll_container_resized():
-	# Update SubViewport size when ScrollContainer is resized
-	if subviewport:
-		subviewport.size = scroll_container.size / zoom_level
-	
 
 func _on_manual_dtree_button_pressed():
 	# Cange visibility
@@ -118,16 +104,6 @@ func _input(event):
 				is_middle_mouse_dragging = false
 			get_viewport().set_input_as_handled()
 		
-		# Handle zoom with Ctrl+Wheel
-		elif event.pressed and event.ctrl_pressed and (event.button_index == MOUSE_BUTTON_WHEEL_UP or event.button_index == MOUSE_BUTTON_WHEEL_DOWN):
-			var zoom_delta = zoom_step if event.button_index == MOUSE_BUTTON_WHEEL_UP else -zoom_step
-			var new_zoom = clamp(zoom_level + zoom_delta, min_zoom, max_zoom)
-			
-			# Apply the new zoom
-			zoom_level = new_zoom
-			subviewport_container.scale = Vector2(zoom_level, zoom_level)
-			subviewport.size = scroll_container.size / zoom_level
-			
 			get_viewport().set_input_as_handled()
 		
 		# Handle mouse wheel scrolling (only if Ctrl is NOT pressed)
