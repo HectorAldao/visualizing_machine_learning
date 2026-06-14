439583866c56c67cf2eb44d62d2724048150a6d9
HectorAldao <ha.31416.contact@proton.me>
2026-06-14 14:47:57 +0200

    feat: cambiado texto en el main menu

--- godot_project/scenes/menus/main_menu.gd
+++ godot_project/scenes/menus/main_menu.gd
@@ -87,10 +87,22 @@ func _update_panel() -> void:
 func _update_text(algorithm: int) -> void:
 	const dict_of_text: Dictionary[int, Array] = {
 		1: ["Árbol de decisión",
-			"El árbol de decisión se construye mediante el algoritmo ID3, creando un modelo de clasificación que divide el conjunto de datos de forma recursiva. En cada nodo de decisión, el algoritmo calcula la entropía del conjunto de datos que llega a él y evalúa la ganancia de información de cada característica categórica disponible. Selecciona el atributo que maximiza esta ganancia de información para realizar la división. El particionamiento continúa en cada rama hasta que todos los ejemplos del nodo pertenecen a la misma clase o se han agotado las características a evaluar."
+			"El árbol de decisión es un modelo que clasifica los datos en base a sus características.
+
+Durante el entranamiento, en los nodos del árbol se decide qué característica de los datos usar para dividirlos de la forma que más se separen.
+Esto es la ganancia de información, y se puede calcular con fórmulas como la entropía.
+El particionamiento continúa en cada rama del árbol hasta que todos los ejemplos del nodo pertenecen a la misma clase o se han agotado las características a evaluar.
+
+Durante el test, los datos descienden por las ramas que indican sus características hasta llegar a un nodo hoja, que determina la clasificación del árbol para ese dato."
 			],
 		2: ["Red neuronal",
-			"Una red neuronal feedforward aproxima funciones complejas mapeando datos a través de capas secuenciales de nodos (neuronas) interconectados. En la fase de propagación hacia adelante (forward propagation), cada nodo calcula el producto escalar de sus entradas y su vector de pesos, añade un término de sesgo (bias), y aplica una función de activación matemática para introducir no linealidad. El entrenamiento se ejecuta mediante el algoritmo de retropropagación (backpropagation): tras evaluar la predicción final mediante una función de error, se calcula el gradiente de este error propagándolo hacia atrás en la red. Finalmente, los pesos y sesgos se actualizan de forma iterativa utilizando el descenso del gradiente para minimizar el error global."
+			"Una red neuronal aproxima una función.
+
+			Está formada por capas, que se conectan una detrás de otra. Estas capas contienen neuronas, que reciben como entrada las salidas de todas las neuronas de la capa anterior y devuelven un valor. Y cada neurona tiene unos números llamados pesos, y tiene tantos como neuronas haya en la capa anterior.
+
+El entrenamiento de una red cuenta con dos fases. La primera es la de propagación hacia adelante. El dato de entranamiento pasa por la red. La salida de la capa final es la predicción de la red para ese dato. La segunda es la retropropagación. En base a cuánto falló la red se calcula cuánto modificar los pesos para que la proxima vez falle menos."
+#La primera fase es la de propagación hacia adelante. Cada neurona calcula la multiplicación de sus entradas por los sus pesos, después los suma junto con un número (llamado bias), y al resultado lo pasa por una función (llamada función de activación).
+#La segunda fase es la retropropagación. De la última capa sale un número por cada neurona en esa capa. Esos números son la predicción de la red. Esta predicción se compara con el valor real que tendría que dar una red neuronal perfecta. Esta comparación se hace con una función de error, que devuelve un número diciendo cuánto ha fallado la red. Con respecto a este error se pueden calcular las derivadas parciales de los pesos de las neuronas. Y con estas derivadas se puede calcular cuánto cambiar los pesos para que la red falle menos.
 			]
 		}
 	
