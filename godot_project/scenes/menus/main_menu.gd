65869f00ef672c65e46632a6187ac602102d9642
HectorAldao <ha.31416.contact@proton.me>
2026-06-15 17:05:13 +0200

    feat: cambiado un poco el texto del árbol de decisión en el main menu

--- godot_project/scenes/menus/main_menu.gd
+++ godot_project/scenes/menus/main_menu.gd
@@ -90,7 +90,7 @@ func _update_text(algorithm: int) -> void:
 			"El árbol de decisión es un modelo que clasifica los datos en base a sus características.
 
 Durante el entranamiento, en los nodos del árbol se decide qué característica de los datos usar para dividirlos de la forma que más se separen.
-Esto es la ganancia de información, y se puede calcular con fórmulas como la entropía.
+Este criterio de división es conocido como 'ganancia de información', y se puede calcular con fórmulas como la entropía.
 El particionamiento continúa en cada rama del árbol hasta que todos los ejemplos del nodo pertenecen a la misma clase o se han agotado las características a evaluar.
 
 Durante el test, los datos descienden por las ramas que indican sus características hasta llegar a un nodo hoja, que determina la clasificación del árbol para ese dato."
@@ -98,7 +98,7 @@ Durante el test, los datos descienden por las ramas que indican sus característ
 		2: ["Red neuronal",
 			"Una red neuronal aproxima una función.
 
-			Está formada por capas, que se conectan una detrás de otra. Estas capas contienen neuronas, que reciben como entrada las salidas de todas las neuronas de la capa anterior y devuelven un valor. Y cada neurona tiene unos números llamados pesos, y tiene tantos como neuronas haya en la capa anterior.
+			Está formada por capas, que se conectan una detrás de otra. Estas capas contienen neuronas, que reciben como entrada las salidas de todas las neuronas de la capa anterior y devuelven un valor. Y cada neurona tiene unos números llamados 'pesos', y tiene tantos como neuronas haya en la capa anterior.
 
 El entrenamiento de una red cuenta con dos fases. La primera es la de propagación hacia adelante. El dato de entranamiento pasa por la red. La salida de la capa final es la predicción de la red para ese dato. La segunda es la retropropagación. En base a cuánto falló la red se calcula cuánto modificar los pesos para que la proxima vez falle menos."
 #La primera fase es la de propagación hacia adelante. Cada neurona calcula la multiplicación de sus entradas por los sus pesos, después los suma junto con un número (llamado bias), y al resultado lo pasa por una función (llamada función de activación).
